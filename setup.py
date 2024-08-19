874e49491a565ebf1a39427a02d8057e8612bfef
huku <huku@grhack.net>
2024-08-19 21:56:08 +0300

    Improve setup.py (Makefile and Makefile.nmake are now obsolete)

--- setup.py
+++ setup.py
@@ -1,7 +1,58 @@
 #!/usr/bin/env python
-from distutils.core import setup
+#
+# When modifying this file, always keep in mind that it has to work on both
+# Python 2.x and 3.x.
+#
 
-setup(name='pyrsistence', version='1.0', description='pyrsistence', author='huku',
-    author_email='huku@grhack.net', url='https://github.com/huku-/pyrsistence',
-    packages=[''], package_data={'': ['pyrsistence.so']})
+import os
+import setuptools
+import sys
 
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+CFLAGS = [
+    "-ggdb",
+    "-Wall",
+    "-Wextra",
+    "-Wno-unused-result",
+    "-Wno-unused-parameter",
+    "-Wno-strict-aliasing",
+    "-Wno-cast-function-type",
+    "-O2",
+    "-fPIC",
+    # "-DDEBUG"
+]
+
+
+def get_sources():
+    return [
+        name
+        for name in os.listdir(DIR)
+        if os.path.isfile(os.path.join(DIR, name)) and name.endswith(".c")
+    ]
+
+
+def main(argv):
+
+    pyrsistence_mod = setuptools.Extension(
+        "pyrsistence",
+        extra_compile_args=CFLAGS,
+        sources=[os.path.join(DIR, name) for name in get_sources()],
+    )
+
+    setuptools.setup(
+        name="pyrsistence",
+        version="1.1",
+        description="pyrsistence",
+        author="huku",
+        author_email="huku@grhack.net",
+        url="https://github.com/huku-/pyrsistence",
+        ext_modules=[pyrsistence_mod],
+    )
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
